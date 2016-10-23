25f0b9b9c8bb0b50b2b35dcf9267d8243f551781
Sergey <sefimenko13@gmail.com>
2016-10-23 15:40:28 +0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,4 +3,11 @@ public class Player{
   private int hp;
   private ArrayList<Card> cardInHand;
   
+  public getHP(){
+    return hp;
+  }
+  
+  public Card playCard(int i){
+    return cardInHand.get(i);
+  }
 }
