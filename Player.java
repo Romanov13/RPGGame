77e38953aa38a734a39dca222c6dd1e22d4aeb45
Sergey <sefimenko13@gmail.com>
2016-10-23 16:11:09 +0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,13 +1,28 @@
 public class Player{
 
-  private int hp;
+  private int hp = 20;
+  private String name; 
   private ArrayList<Card> cardInHand;
   
+  Player(String n){
+    name = n;
+  }
+    public getName(){
+    return name;
+  }
   public getHP(){
     return hp;
   }
   
+  public void addACard(Card){
+   cardInHand.add(Card); 
+  }
+  
   public Card playCard(int i){
     return cardInHand.get(i);
+  }
+  
+  public reduceHP(int d){
+    hp = hp - d;
   }
 }
